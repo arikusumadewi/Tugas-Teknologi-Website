4195b067096ae8b1ea8cfb193184470a882d904c
arikusumadewi <114129833+arikusumadewi@users.noreply.github.com>
2022-11-20 18:13:53 +0700

    Create aritmetika.blade.php

---
+++ aritmetika.blade.php
@@ -0,0 +1,24 @@
+<!DOC TYPE html>
+<html lang="en">
+
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale-1.0"> 
+    <meta http-equiv="X-UA-Compaatible" content="ie=edge">
+    <title> Operasi Aritmatika</title>
+</head>
+<body> 
+    <h1>Operasi Aritmatika</h1>
+    <form method="GET" action="latihan">
+        Nilai a : <input type="text" name="a"><br><br>
+        Nilai b : <input type="text" name="b"><br><br>
+        operasi : <br>
+        <input type="radio" name="operasi"
+        value="jumlah">Penjumlahan<br>
+        <input type="radio" name="operasi"
+        value="modulus">Modulus<br>
+        <input type="radio" name="operasi"
+        value="kali">Perkalian<br> 
+        <br> 
+        <input type="submit" value="hasil">
+    </form>
